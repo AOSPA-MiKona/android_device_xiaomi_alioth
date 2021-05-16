1e523c3ea81314bd5ec392ccf9b03e16deecd6fe
Parth Bhatia <parthbhatia98@gmail.com>
2021-05-16 15:39:59 -0300

    alioth: overlay: Enable Double Tap to wake gesture
    Signed-off-by: Mesquita <mesquita@aospa.co>

--- device.mk
+++ device.mk
@@ -43,6 +43,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-pa
 
+PRODUCT_PACKAGES += \
+    AOSPAAliothFrameworks
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
